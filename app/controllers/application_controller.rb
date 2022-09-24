fa4fb3ffa2aab1bc6c52938c61a504c4e25fb5b5
okazawayu <gachizawa@gmail.com>
2022-09-24 13:11:00 +0900

    ユーザ管理機能実装_1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
-
-
-  private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '2222'
-    end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+ 
+  protected
+ 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:nickname, :birth_date, :first_name, :last_name, :first_name_kana, :last_name_kana]
+    )
   end
-end
+ end
---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         VALID_ZENKAKU_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
+         VALID_KATAKANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+         
+         validates :nickname, presence: true
+         validates :first_name, presence: true
+         validates :first_name, format: { with: VALID_ZENKAKU_REGEX, message: ' must be entered in double-byte characters'}, allow_blank: true
+         validates :last_name, presence: true
+         validates :last_name, format: { with: VALID_ZENKAKU_REGEX, message: ' must be entered in double-byte characters'}, allow_blank: true
+         validates :first_name_kana, presence: true
+         validates :first_name_kana, format: { with: VALID_KATAKANA_REGEX, message: ' must be entered in katakana'}, allow_blank: true
+         validates :last_name_kana, presence: true
+         validates :last_name_kana, format: { with: VALID_KATAKANA_REGEX, message: ' must be entered in katakana'}, allow_blank: true
+         validates :birth_date, presence: true
+         validates :password, format: { with: VALID_PASSWORD_REGEX, message: ' must contain both single-byte alphanumeric characters'}, allow_blank: true
+         
+end
